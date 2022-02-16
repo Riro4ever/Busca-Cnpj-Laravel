a07086c94ff0f835e86e4207dc2e1d844a738991
Ricardo Araújo <ricardoriro@live.com>
2022-02-16 20:26:47 -0300

    Inserindo validações e mensagens de erro baásicos

--- app/Http/Controllers/ReceitaFederalController.php
+++ app/Http/Controllers/ReceitaFederalController.php
@@ -3,17 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class ReceitaFederalController extends Controller
 {
     function getCNPJ($cnpj) {
 
+        $cnpj_formatado = preg_replace("/[^0-9]/", "", $cnpj);
 
-        $url = "https://brasilapi.com.br/api/cnpj/v1/" . preg_replace("/[^0-9]/", "", $cnpj);
+        if(!$cnpj_formatado || strlen($cnpj_formatado) != 14) {
+          return response()->json([
+            'message' => 'CNPJ em formato inválido!'], 400);
+        }
 
-        $response = file_get_contents("https://brasilapi.com.br/api/cnpj/v1/$cnpj");
+        $url = "https://brasilapi.com.br/api/cnpj/v1/" . $cnpj_formatado;
 
-        return $response;
+        $client = new Client();
+
+        try {
+          $res = $client->get($url);
+        } catch (\GuzzleHttp\Exception\ClientException $e) {
+          return response()->json([
+            'message' => $res->getBody()], $res->getStatus());
+        }
+
+        
+
+
+
+        return $res->getBody();
+
 
     }
 }
